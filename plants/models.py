d7740204187debcb715f6bb747a44b19b48accf7
Alan <todosesolucionara@protonmail.com>
2021-10-18 21:46:24 +0200

    Varios cambios

--- plants/models.py
+++ plants/models.py
@@ -6,9 +6,22 @@ def file_directory_path(instance, filename):
 
 class Plant(models.Model):
 	name=models.CharField(max_length=200, null=False, blank=False)
+	nameComun=models.CharField(max_length=200,null=True)
 	description=models.TextField(null=True)
 	image=models.ImageField(upload_to=file_directory_path, null=True)
 	riego=models.IntegerField(null=True)
+	tierra=models.CharField(
+		max_length=3,
+		choices=[
+			('TIE',"Tierra"),
+			('ARE',"Arenosa"),
+			('ARC',"Arcillosa"),
+			('LIM',"Limosa"),
+			('TUB',"Turba"),
+			('TIZ',"Tiza")
+		],
+		default='TIE'
+	)
 
 	def __str__(self):
 		return self.name
